64819de3fc668d4792830ea3b27e7468ab22a6ba
wojciech balcerzak <wojciech.balcerzak@orpeg.pl>
2020-05-11 21:11:35 +0200

    lekcja 3 wyr kl7

---
+++ SP8/Lekcja9/pierwszy.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+float a, b;
+cout << "podaj liczbê a = ";
+cin >> a;
+cout << "podaj liczbê b =";
+cin >> b;
+	
+	cout << " obliczamy iloraz b/a  \n";
+	float div = b/a;
+	cout << "iloraz b/a = " << div << endl;
+	
+	cout << "dzielenie liczb 25/10.0 = "<< 25/10.0;
+		
+	return 0;
+}

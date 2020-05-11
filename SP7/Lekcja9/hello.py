872383ffae9e8cd6c8a617c0ec1feef6ae3f6e10
Wojciech Balcerzak <wbalcerz@gmail.com>
2020-05-11 21:41:05 +0200

    lekcja 9

---
+++ SP7/Lekcja9/hello.py
@@ -0,0 +1,4 @@
+print("liczymy pole prostokąta o bokach a i b")
+a = int(input("podaj dlugość boku a  "))
+b = int(input("podaj dlusc boku b  "))
+print("pole prostokąta o bokach a =", a, "oraz b = ", b, "wynosi P = ", a*b)

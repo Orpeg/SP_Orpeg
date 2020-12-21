cc006c4529c67a958271a2d29e7dd0ec60a9eae7
Wojciech Balcerzak <wbalcerz@gmail.com>
2020-12-21 19:18:13 +0100

    lekcja 4

---
+++ SP8/Lekcja4/Lekcja4_kl8.py
@@ -0,0 +1,10 @@
+# Typ zmiennej liczbowy
+a = 4
+b = 5
+print('zmienna a =', a)
+print('zmienna b=', b)
+suma = a+b
+print('zmienna suma=', suma)
+# zmienne tekstowe (łańcuchowe)  string
+napis = 'Dzień dobry'
+print(napis)

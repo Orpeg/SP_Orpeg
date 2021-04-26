8967537f2602408a9249d00aa1fedee3e2e0ee3b
Wojciech Balcerzakf <wbalcerz@gmail.com>
2021-04-26 23:13:59 +0200

    lekcja 8

---
+++ SP7/Lekcja8/pierwszy_python.py
@@ -0,0 +1,17 @@
+# zmienne w pythonie
+
+a = int(input('Podaj liczbę całkowitą a '))
+
+print('a = ', a)
+b = int(input('Podaj liczbę całkowitą b '))
+
+suma = a+b
+print('b = ', b)
+print('liczymy sumę')
+
+print('suma liczb a + b = ', suma)
+
+print('Jaką część sumy stanowi liczba a ? ')
+
+x = a/suma
+print('a/suma = ',x)
---
+++ SP8/Lekcja8/raz_dwa.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int main(){
+
+int a, b;
+int suma;
+
+
+std::cout << "Podaj zmienna a " << std::endl;
+std::cin >> a;
+std::cout << "Podaj zmienna b " << std::endl;
+std::cin >> b;
+
+suma = a+b;
+
+std::cout << "suma zmiennych a + b wynosi  " << suma << std::endl;
+
+
+
+return 0;
+
+}

d6713989e617ba219d312ee8fcff0e53c885dee0
Wojciech Balcerzakf <wbalcerz@gmail.com>
2021-12-07 20:57:18 +0100

    update

---
+++ 2021/Klasa 8/begin.py
@@ -0,0 +1,21 @@
+# print('Witajcie uczniowie klasy 8')
+# zmienne integer
+a = 2
+b = 3 
+print('zmienna a = ', a)
+print('zmienna b = ', b)
+print('suma zmiennych a+b = ', a+b)
+
+# zmienne typu float
+
+print('Ile wynosi liczba pi?')
+pi = 3.1415926536
+
+print('Liczba pi = ', pi)
+# liczy pole koła
+
+print('Powierzchnia koła o promieniu 3 wynosi ',end=' ' )
+r = 3
+pole = pi * r * 4
+
+print(pole)

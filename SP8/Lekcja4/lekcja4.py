5149a8d1625fd424b5b5554290e7c502bd534f1c
Wojciech Balcerzak <wojciech.balcerzak@orpeg.pl>
2022-12-07 19:40:01 +0100

    update

---
+++ SP8/Lekcja4/lekcja4.py
@@ -0,0 +1,65 @@
+# coding: utf-8
+# zmienne
+a = 2
+b = 3
+print(a)
+print(b)
+c = 'Justyna'
+print(c)
+x = b/c
+b
+a
+x = b/
+x = b/a
+print(x)
+print('suma zmiennych a + b =',a+b)
+print('suma zmiennych a + b = '+a+b)
+print(f'suma liczb a+b = {a+b}')
+#float
+x
+print('x:.2f')
+print({x:,2f})
+print({x:0.2f})
+print({x:2f})
+print(10/3)
+print(f'{10/3:.2f}')
+print(f'{10/3:.5f}')
+a = 0
+k = 0.05
+for i in range(1,20):
+    print(f'{i},  {a+k}')
+    
+for i in range(1,20):
+    a = a+k
+    print(f'{i},  {a}')
+    
+for i in range(1,20):
+    a = a+k
+    print(f'{i}.  {a}')
+    
+a=0
+for i in range(1,20):
+    a = a+k
+    print(f'{i}.  {a}')
+    
+k
+k = 0.125
+k*8
+for i in range(1,20):
+    a = a+k
+    print(f'{i}.  {a}')
+    
+a=0
+for i in range(1,20):
+    a = a+k
+    print(f'{i}.  {a}')
+    
+for i in range(1,20):
+    a = a+k
+    print(f'{i}.  {a:.3f}')
+    
+a =0
+for i in range(1,20):
+    a = a+k
+    print(f'{i}.  {a:.3f}')
+    

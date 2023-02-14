4e9a71c5930a830fa5e7409eee7c525b0febdebf
Wojciech Balcerzak <wojciech.balcerzak@orpeg.pl>
2023-02-14 20:53:41 +0100

    up

---
+++ SP8/Lekcja5/lekcja5bis.py
@@ -0,0 +1,99 @@
+# coding: utf-8
+#zmiene tyipy
+a = 2
+x = 3.0
+#licaba float
+bin(10)
+i = 0
+x =0.1
+while i<20:
+    x+=0.1
+    i+=1
+    print(f'dla i = {i} wartość x = {x}')
+    
+cena = 10000
+ilosc = 35
+cena_jedn = cena/ilosc
+cena_jedn
+285.71*35
+285.72*35
+285.72*35
+1000/1.23
+813.01*1.23
+while i<20:
+    x+=0.125
+    i+=1
+    print(f'dla i = {i} wartość x = {x}')
+    
+i = 0; x =0.0
+while i<20:
+    x+=0.125
+    i+=1
+    print(f'dla i = {i} wartość x = {x}')
+    
+bool(1)
+bool(0)
+a = 2
+b = 2
+a==b
+c = 3
+a == c
+napis = 'W pustyni i w puszczy'
+napis
+napis.upper()
+napis.lower()
+napis.title()
+napis.startswith('W')
+napis.startswith('w')
+napis.endswith('y')
+napis.endswith('puszczy')
+napis[::-1]
+napis[2:10:1]
+napis[10:2:-1]
+napis[9:1:-1]
+napis[::2]
+len(napis)
+napis[::2]+napis[1:2]
+napis[0]
+napis[-1]
+napis[-2]
+napis[1]
+list(napis)
+naems = ['Jurek','Małgosia', 'Jaś', 'Katarzyna']
+names
+names = ['Jurek','Małgosia', 'Jaś', 'Katarzyna']
+names
+age = [12, 14, 9, 6]
+slow = {names, age}
+dic(names, age)
+dict(names, age)
+dict({names, age}]
+dict({names,age})
+zip(names, age)
+dict(zip(names, age))
+(2,3,4,10)
+x = (2,3,4,10)
+a,b,c,d = x
+a
+b
+c
+d
+a,b
+a,b = b,a
+a,b
+2*3
+2**3
+#a2 + b@ = c2
+a,b = 2,3
+c = (a**2+b**2)**(.5)
+c
+sqrt(2)
+import math
+math.sqrt(2)
+from math import sqrt
+sqrt(2)
+from math import pi
+pi
+r =3
+pole = pi*r**2
+pole

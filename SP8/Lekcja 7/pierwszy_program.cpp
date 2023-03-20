533cc8d18760afdc330eb67b2f33f130b747c4d8
Wojciech Balcerzak <wojciech.balcerzak@orpeg.pl>
2023-03-20 10:08:21 +0100

    upda

---
+++ SP8/Lekcja 7/pierwszy_program.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+//	pytamy o imię i wiek
+	string name;
+	int wiek;
+	cout << "Jak masz na imie?  "<< "\n";
+	cin>> name;
+	cout << "Ila masz lat ? "<< "\n";
+	cin >> wiek;
+	cout << "jak sie masz "<<name<<"? "<<"\n";
+	cout <<"o! masz "<< wiek << " lat, jestes juz duzy  "<< "\n";
+	cout << "witamy w pierszym programie "<<"\n";
+	
+	
+	
+	
+	return 0;
+}
